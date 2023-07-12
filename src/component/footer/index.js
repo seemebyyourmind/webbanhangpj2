0a97ee2da61203d7c79c121bd651845d3335d40e
Hoang <phamdinhhoang152000@gmail.com>
2023-07-12 21:24:47 +0700

    khoitao

---
+++ src/component/footer/index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function Footer() {
+  return (
+    <div className="footer--component">
+      <p className="Footer">Summmer deal for swimpool </p>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
---
+++ src/component/header/index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function Header() {
+  return (
+    <div>
+      <p className="">Summmer deal for swimpool </p>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
